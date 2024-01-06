df4c2ee06fab722ed651c5bc89a6631dc3c1ee39
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-01-06 20:04:46 +0000

    wrap in function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 let table = document.querySelector('table');
 
-
-for (let i = 1; i <= 10; i++) {
-    let row = table.insertRow();
-    for (let j = 1; j <= 10; j++) {
-        let cell = row.insertCell();
-        cell.innerHTML = (i - 1) * 10 + j;
+function createTable() {
+    for (let i = 1; i <= 10; i++) {
+        let row = table.insertRow();
+        for (let j = 1; j <= 10; j++) {
+            let cell = row.insertCell();
+            cell.innerHTML = (i - 1) * 10 + j;
+        }
     }
 }
+
+createTable();
 
 
